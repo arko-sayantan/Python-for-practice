bc1d3900ebacd37f4734eedef8b09b3fc5e1ee22
Sayantan Das <63104414+arko-sayantan@users.noreply.github.com>
2021-01-11 21:01:34 +0530

    Update List coprehension.py

--- List coprehension.py
+++ List coprehension.py
@@ -12,7 +12,10 @@ mylist = [1,2,3,4,5,6,7,8]
 mylis = [x**2 for x in mylist]  # The for-loop operation take place in oneline
                                 # this is the advance of list comprehension 
                                 # x**2 is the opretion which take place in the for loop
+mylis2 = [x**2 for x in mylis if x%2 == 0] # It prints square of odd position of elements in list 
+                                            # you can use if-else in list comprehension.
 print(f'The square of the element in mylis: {mylis}')
+print(f'The square of odd element in mylist :{mylis2}')
 
 celsius = [10,20,40,30,40,50,99]
 fahrenteit = [((9/5)*temp+32) for temp in celsius] # (9/5)*temp+32) --> main operation
